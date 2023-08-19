005a72def3f69d0c2cfcc2a1ad0d4299db1fe3f0
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-08-19 14:35:04 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world');
+import { config } from "dotenv";
+import express from "express";
+
+config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+const port = process.env.PORT || 3036;
+
+app.listen(port, () =>
+  console.log(`server running on port http://localhost:${port}`),
+);
