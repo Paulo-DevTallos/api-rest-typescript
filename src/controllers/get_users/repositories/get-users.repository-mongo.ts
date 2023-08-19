4a47a9a997c3f7a0cc392b7048944c21a0811aed
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-08-19 15:53:24 -0300

    feat: ensure get users mongo repository gets user from mongo

--- src/controllers/get_users/repositories/get-users.repository-mongo.ts
+++ src/controllers/get_users/repositories/get-users.repository-mongo.ts
@@ -1,15 +1,19 @@
+import { MongoClient } from "../../../database";
 import { User } from "../../../models/User";
 import { GetUsersReposiroty } from "./get-users.repository";
 
 export class GetUsersReposirotyMongo implements GetUsersReposiroty {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Paulo",
-        lastName: "Sergio",
-        email: "paulo@email.com.br",
-        password: "123456",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    // converter id com underline para o padrão da model de User
+    // ({_id, ...rest}) => remove o _id e retorna todo o resto ...rest
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
